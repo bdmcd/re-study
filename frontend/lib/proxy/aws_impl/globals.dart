b5e762327539c8bc46da02d27726d13710bd06ed
Brian McDaniel <brian.mcdaniel@legrand.us>
2020-03-02 16:53:39 -0700

    Create the rest of the mock proxies

---
+++ frontend/lib/proxy/aws_impl/globals.dart
@@ -0,0 +1,10 @@
+class AWSGlobals {
+  static const String baseUrl = "";
+  static const String registerUrl = baseUrl + "/register";
+  static const String getSetsUrl = baseUrl + "/getSets/{userGuid}";
+  static const String createSetUrl = baseUrl + "/createSet";
+  static const String updateSetUrl = baseUrl + "/updateSet";
+  static const String getCardsUrl = baseUrl + "/getCards/{setGuid}";
+  static const String createCardUrl = baseUrl + "/createCard";
+  static const String updateCard = baseUrl + "/updateCard";
+}
--- frontend/lib/proxy/mock_impl/mock_card_proxy.dart
+++ frontend/lib/proxy/mock_impl/mock_card_proxy.dart
@@ -2,6 +2,7 @@
 import 'package:restudy/model/flash_card.dart';
 import 'package:restudy/proxy/card_proxy.dart';
 import 'package:restudy/request/create_flashcard_request.dart';
+import 'package:restudy/request/update_flashcard_request.dart';
 
 class MockCardProxy implements CardProxy {
   @override
@@ -15,6 +16,19 @@ class MockCardProxy implements CardProxy {
       question: request.question,
       answer: request.answer,
       retentionScore: 0,
+    );
+  }
+
+  @override
+  Future<Flashcard> updateCard(UpdateFlashcardRequest request) async {
+    await Future.delayed(Duration(milliseconds: 200));
+    
+    return Flashcard(
+      id: request.cardId,
+      creatorId: "mock_creator_id",
+      setId: request.setId,
+      question: request.question,
+      answer: request.answer,
     );
   }
 }
