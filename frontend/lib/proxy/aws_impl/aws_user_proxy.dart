a90a0f97f816213a108694fe840bdb192406fd36
Justin Hendrickson <justintunev7@gmail.com>
2020-03-09 15:36:51 -0600

    cleanup proxy requests

--- frontend/lib/proxy/aws_impl/aws_user_proxy.dart
+++ frontend/lib/proxy/aws_impl/aws_user_proxy.dart
@@ -7,17 +7,6 @@ class AWSUserProxy implements UserProxy {
   @override
   Future<List<FlashcardSet>> getSets(GetSetsRequest request) async {
     // TODO: implement getSets
-    await Future.delayed(Duration(milliseconds: 200));
-
-    final mockSets = [
-      FlashcardSet(name: "Biology", creatorId: "1", id: "1"),
-      FlashcardSet(name: "Chemistry", creatorId: "2", id: "2"),
-      FlashcardSet(name: "CS 428", creatorId: "3", id: "3"),
-      FlashcardSet(name: "English", creatorId: "4", id: "4"),
-      FlashcardSet(name: "Spanish", creatorId: "5", id: "5"),
-      FlashcardSet(name: "Book of Mormon", creatorId: "6", id: "6"),
-    ];
-    return mockSets;
-    // return null;
+    return null;
   }
 }
